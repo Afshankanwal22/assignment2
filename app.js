cc22c1ff96fe4d07b2932d1fbb031e609e5e1cc4
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-02-04 03:20:41 +0500

    assignment2

---
+++ app.js
@@ -0,0 +1,104 @@
+// Qno:1 Write a program that take two numbers & add them in a new variable. Show the result in your browser?
+// let num1 = 3
+// let num2 = 5
+// console.log(num1+num2);
+// document.write ("sum of " +num1 + " and " + num2 + " is " + 8) 
+
+// Qno:2 Repeat task1 for subtraction, multiplication, division & modulus?
+//    let num1 = 3
+//    let num2 = 5
+//    console.log(num1-num2);
+
+//    let num1 = 3
+//    let num2 = 5
+//    console.log(num1*num2);
+
+// let num1 = 3
+// let num2 = 5
+// console.log(num1/num2);
+
+// let num1 = 3
+// let num2 = 5
+// console.log (num1%num2);
+
+// Qno:3 Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value
+// after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”?
+
+// let num1;
+// console.log(num1);
+// document.write("Value after variable declaration is " + num1 );
+// num1 = 5
+// console.log(num1);
+// document.write("<br/ > Inital value:" + num1)
+// let newNum = ++num1;
+// console.log(newNum);
+// document.write("<br /> Value after increment is: " +newNum)
+// let num2 = 7
+// let newAdd= newNum+num2
+// console.log(newAdd);
+// document.write("<br /> Value after addition is: " +newAdd)
+// let newSub = --newAdd
+// console.log(newSub);
+// document.write("<br /> Value after decrement is: " + newSub)
+
+
+// Qno:4 Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output?
+// let movieTicket = 600
+// let buyTicket = 5
+// let totalPrice = 600*5
+// console.log(totalPrice)
+// document.write("Total cost to buy " + buyTicket + " tickets to a movie is " +totalPrice +" PKR")
+
+// Qno:5 Write a script to display multiplication table of any number in your browser?E.g
+// document.write("Table of "+4)
+
+// Qno:6 Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store
+// ]the following in variables?
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+
+// Compute the total cost & show the receipt in your browser.
+let priceItem = 
+let 
+let 
+let
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
