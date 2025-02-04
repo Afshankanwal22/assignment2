68f7c90b607a42952c4ffc66f98a37dd556bd99b
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-02-05 03:02:50 +0500

    assignment2

--- app.js
+++ app.js
@@ -73,7 +73,7 @@
 // Qno:5 Write a script to display multiplication table of any number in your browser?E.g
 // document.write("Table of "+4)
 
-// Qno:6 Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store
+// Qno:7 Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store
 // ]the following in variables?
 // a. Price of item 1
 // b. Price of item 2
@@ -82,10 +82,100 @@
 // e. Shipping charges
 
 // Compute the total cost & show the receipt in your browser.
-let priceItem = 
-let 
-let 
-let
+// let priceItem = 
+// let 
+// let 
+// let
+
+// Qno:8 Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser?
+// let totalMarks = 980
+// let marksObtained = 804
+// let percentage = marksObtained/totalMarks*100
+// console.log(percentage)
+// document.write("<h1>Marks Sheet </h1>")
+// document.write("Total marks: " + 980)
+// document.write("<br /> Marks obtained: " +804)
+// document.write ("<br /> percentage: " + percentage)
+
+// Qno:9 Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees.
+// Perform all calculations in a single expression?
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee
+// and 1 Saudi Riyal = 28 Pakistani Rupee).
+// let usDolars = 10*104.80
+// let saudiRiyals = 25*28
+// let totalCurrency = usDolars+ saudiRiyals
+// console.log(totalCurrency);
+// document.write("<h1>Currency in PKR</h1>")
+// document.write(" Total currency in PKR: "+totalCurrency)
+
+// Qno:10 Write a program to initialize a variable with some number and do arithmetic in following sequence?
+// a. Add 5
+// b. Multiply by 10
+// c. Divide the result by 2
+// Perform all calculations in a single expression.
+
+
+// Qno:11 The Age Calculator: Forgot how old someone is? 
+// Calculate it!
+// a. Store the current year in a variable.
+// b. Store their birth year in a variable.
+// c. Calculate their 2 possible ages based on the stored
+// values.
+
+// Output them to the screen like so: “They are either NN or NN
+// years old”.
+// let currentYear = 2016
+// let birthYear = 1992
+// let yourAge = 24
+// console.log(currentYear,birthYear,yourAge)
+// document.write("<h1>Age Calculator</h1>")
+// document.write("Current Year: "+currentYear)
+// document.write("<br /> Birth Year: "+birthYear)
+// document.write("<br /> Your Age is: "+yourAge)
+
+// Qno:12 The Geometrizer: Calculate properties of a circle?
+// a. Store a radius into a variable.
+// b. Calculate the circumference based on the radius, and
+// output “The circumference is NN”.
+// (Hint : Circumference of a circle = 2 π r , π = 3.142)
+// Calculate the area based on the radius, and output “The
+// area is NN”. (Hint : Area of a circle = π r2, π = 3.142).
+
+
+// Qno:13 The Lifetime Supply Calculator: Ever wonder how much a “lifetime supply” of your favorite snack is?
+// Wonder no more.
+// a. Store your favorite snack into a variable
+// b. Store your current age into a variable.
+// c. Store a maximum age into a variable.
+// d. Store an estimated amount per day (as a number).
+// e. Calculate how many would you eat total for the rest of
+// your life.
+// Output the result to the screen like so: “You will need
+// NNNN to last you until the ripe old age of NN”.
+// let favoriteSnack = "chocolate chip"
+let currentAge = 15
+let estimatedMax = 65
+let amountPerDay = 3
+let chocolateChip = 150
+let oldAge = 65
+// console.log(favoriteSnack,currentAge,estimatedMax,amountPerDay,chocolateChip,oldAge)
+let totalRest = estimatedMax- currentAge
+console.log(totalRest)
+// document.write("<h1> The Lifetime Supply Calculator</h1>")
+// document.write("Favourite Snack: "+favoriteSnack)
+// document.write("<br /> Current Age: "+currentAge)
+// document.write("<br />Estimated Maximum Age: "+estimatedMax)
+// document.write("<br />Amount of snack per day: "+amountPerDay)
+// document.write("<br />You will need "+chocolateChip + "chocolate chip to last you until the ripe old age of "+oldAge)
+
+
+
+
+
+
+
+
+ 
 
 
 
